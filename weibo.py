f5949584d8303cc0ce3404c72853604092223af4
Meng An <isolate000@gmail.com>
2016-05-26 09:55:44 +0800

    change weibo

--- weibo.py
+++ weibo.py
@@ -5,21 +5,39 @@ import base64
 from bs4 import BeautifulSoup
 import sys
 import time
+import datetime
+import hashlib
 import traceback
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 fname = 'data/weibo.txt'
 
+INDEX = 0
+INDEX = 1
+
 QT = [
-    {'url':'http://s.weibo.com/top/summary?cate=realtimehot','s1':'实时热搜榜'},
+    #{'url':'http://s.weibo.com/top/summary?cate=realtimehot','s1':'实时热搜榜'},
+    {'url':'http://s.weibo.com/top/summary?cate=total&key=friends','s1':'好友热搜榜'},
 ]
 
+def save_to_ys(i):
+    try:
+        url = 'http://101.200.130.178/crawler/Weibotop/'
+        r = requests.post(url, auth=('admin', 'brotec'), json=i)
+        return '%d' % r.status_code
+    except Exception as e:
+        err = traceback.format_exc()
+        print(err)
+        return err
+    return 'failed'
 def get_all_item():
     try:
         s = requests.Session()
         for u in QT:
             url = u['url']
+            s1 = u['s1']
+
             r = s.get(url)
             print(r.status_code)
             soup = BeautifulSoup(r.text)
@@ -27,6 +45,7 @@ def get_all_item():
             s = soup.find_all('script')[-2].string
 
             start = 0
+            rank = 1
             while True:
                 try:
                     td01_1 = s.index('td_01', start) - 12
@@ -38,17 +57,26 @@ def get_all_item():
                 soup_item = BeautifulSoup(item)
                 #print(soup_item.prettify())
                 item= soup_item.get_text().replace('\\n', '').split()
-                print(item)
-                rank = item[0]
+                #print(item)
                 word = item[1]
-                value = item[2]
+
+                i = {}
+                i['rank'] = '%d' % rank
+                i['word'] = word.decode('unicode-escape')
+                i['s1'] = s1
+                i['date_str'] = datetime.datetime.today().strftime('%Y-%m-%d')
+                uuid_str = i['date_str']+i['s1']+i['rank']
+                i['uuid'] = hashlib.sha1(uuid_str.encode('utf-8')).hexdigest()
+
+                r = ''
+                #r = save_to_ys(i)
+                print(i['date_str']+' '+i['s1']+' '+i['rank']+' '+i['word']+' '+r)
                 #rank = item[0].rtrip('\\n')
                 #word = item[1].rtrip('\\n')
                 #value = item[2].rtrip('\\n')
-                print(rank)
-                print(word.decode('unicode-escape'))
-                print(value)
+                #print(word.decode('unicode-escape'))
 
+                rank += 1
                 start = td01_2
 
 
